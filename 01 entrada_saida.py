3c893dcc8d3e68fbf281d34c0bd5c4ca9098665a
Fabio Geraldini <fabio.ags@hotmail.com>
2025-04-07 19:20:47 -0300

    teste git

---
+++ 01 entrada_saida.py
@@ -0,0 +1,8 @@
+nome = input("digite seu nome: ")
+idade = int(input("digite sua idade: "))
+
+ano = 2023 - idade
+
+mensagem = f"olá, seu nome é {nome} e vocë nasceu no ano de {ano}"
+
+print(mensagem)
